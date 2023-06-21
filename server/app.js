f8ea12f36a3124a75a1d27479fdd0588622949b4
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-06-21 14:51:16 -0300

    Se completó el codigo faltante en app.js: se importaron middlewares y se implementaron

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet')
+
 
 const path = require('path');
 
@@ -8,6 +11,12 @@ const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet())
+app.use(express.json());
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.get('*', (_req, res) => {
+    res.status(404).send('Error 404 - La peticion no coincide con ninguna de las rutas declaradas');
+})
 
 // Starting the server
 app.listen(45635, () => console.log('Server on port xxxx'));
